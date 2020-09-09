7d720ea7530afe66fe25eb8a0a3413a0609f0e6a
JRivera131313 <71032064+JRivera131313@users.noreply.github.com>
2020-09-09 17:14:18 -0500

    Fixed brackets and commets

--- listDriver.cpp
+++ listDriver.cpp
@@ -10,16 +10,15 @@
 using namespace std;
 void PrintList(ofstream& outFile, UnsortedType& list);
 
-int main()
-{
+int main(){
 
   ifstream inFile;       // file containing operations
   ofstream outFile;      // file containing output
   string inFileName;     // input file external name
   string outFileName;    // output file external name
-  string outputLabel;     
+  string outputLabel;
   string command;        // operation to be executed
-  
+
   int number;
   ItemType item;
   UnsortedType list;
@@ -39,84 +38,86 @@ int main()
   cout << "Enter name of test run; press return." << endl;
   cin  >> outputLabel;
   outFile << outputLabel << endl;
-  if (!inFile)
-  {
+  if (!inFile){
     cout << "file not found" << endl;
 	exit(2);
   }
+
   inFile >> command;
 
   numCommands = 0;
-  while (command != "Quit")
-  { 
-    if (command == "PutItem")
-    {
-      inFile >> number; 
+  while (command != "Quit"){ 
+    if (command == "PutItem"){
+      inFile >> number;
       item.Initialize(number);
       list.PutItem(item);
       item.Print(outFile);
       outFile << " is in list" << endl;
     }
-    else if (command == "DeleteItem")
-    {
+
+    else if (command == "DeleteItem"){
       inFile >> number;
       item.Initialize(number);
       list.DeleteItem(item);
       item.Print(outFile);
       outFile << " is deleted" << endl;
     }
-    else if (command == "GetItem")
-    {
+
+    else if (command == "GetItem"){
       inFile >> number;
       item.Initialize(number);
       item = list.GetItem(item, found);
       item.Print(outFile);
+
       if (found)
         outFile << " found in list." << endl;
-      else outFile <<  " not in list."  << endl;  
-    } 
-    else if (command == "GetLength")  
+
+      else outFile <<  " not in list."  << endl;
+    }
+    else if (command == "GetLength"){
       outFile << "Length is " << list.GetLength() << endl;
-    else if (command == "IsFull")
+    }
+    else if (command == "IsFull"){
       if (list.IsFull())
         outFile << "List is full." << endl;
-      else outFile << "List is not full."  << endl;  
-    else if (command == "MakeEmpty")
-	  list.MakeEmpty();
-	else if (command == "PrintList")
+      else outFile << "List is not full."  << endl;
+    }
+    else if (command == "MakeEmpty"){
+	     list.MakeEmpty();
+    }
+	  else if (command == "PrintList"){
 	  PrintList(outFile, list);
-	else
+  }
+	  else{
 	  cout << command << " is not a valid command." << endl;
+    }
 	numCommands++;
-    cout <<  " Command number " << numCommands << " completed." 
-         << endl;
-    inFile >> command;
+  cout <<  " Command number " << numCommands << " completed."
+        << endl;
+  inFile >> command;
   };
- 
+
   cout << "Testing completed."  << endl;
   inFile.close();
   outFile.close();
   return 0;
 }
 
-
-void PrintList(ofstream& dataFile, UnsortedType& list)
-// Pre:  list has been initialized.      
-//       dataFile is open for writing.   
+// Pre:  list has been initialized.
+//       dataFile is open for writing.
 // Post: Each component in list has been written to dataFile.
-//       dataFile is still open.         
-{
+//       dataFile is still open.
+void PrintList(ofstream& dataFile, UnsortedType& list){
   int length;
   ItemType item;
 
   list.ResetList();
   length = list.GetLength();
-  for (int counter = 1; counter <= length; counter++)
-  {
+
+  for (int counter = 1; counter <= length; counter++){
     item = list.GetNextItem();
     item.Print(dataFile);
     dataFile << " ";
   }
   dataFile << endl;
 }
-
